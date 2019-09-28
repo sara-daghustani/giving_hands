2014c2ac98329b5c5f5e4d86eff4455ccc9bdc08
Sara Daghustani <sara.daghustani@gmail.com>
2019-09-28 17:20:34 +0300

    fixed signup form and add nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'pages/home'
+  root "pages#home"
+
+  get "/home", to: "pages#home"
+
   resources :organization_profiles
   resources :volunteer_profiles
   resources :events
-  root "pages#home"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
